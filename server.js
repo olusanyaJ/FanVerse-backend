1b1aa904b0bbd57b212a2c909b2f7e0a596852a9
Olusanya Jegede <olusanyajegede1@gmail.com>
2024-03-20 13:57:35 -0500

    connect app to db

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 8000;
+require("./src/config/db");
 
-app.get("/", (req, res) => {
-  res.send("FanVerse Backend is running!");
+const express = require("express");
+const bodyParser = express.json;
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser);
+
+const PORT = process.env.PORT;
+
+app.get("/", (_req, res) => {
+  try {
+    res.send("FanVerse Backend is running!");
+  } catch (err) {
+    res.status(400).send(`Error retrieving: ${err}`);
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`server running at http://localhost:${PORT}`);
 });
